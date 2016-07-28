72db23f9eb97ac94804adb3ef1e052943d8447b8
Bernhard Bliem <bliem@dbai.tuwien.ac.at>
2016-07-28 17:35:03 +0200

    Added warnings if there is no unique best match with decent score

--- download-liked-papers.rb
+++ download-liked-papers.rb
@@ -10,7 +10,7 @@ require 'fileutils'
 def download_paper(id, title, dir)
   filename = "#{dir}/#{title}.pdf"
   if File.exist?(filename)
-    puts "Skipping paper: File '#{filename}' already exists"
+    warn "Skipping paper: File '#{filename}' already exists"
     return
   end
 
@@ -74,8 +74,20 @@ def download_liked_papers(dir)
     paper = @confer_papers[id]
     #next if paper['type'] != 'paper'
     next if paper['abstract'].empty?
-    scores = Amatch::LongestSubsequence.new(paper['title']).match(@proceedings_titles)
+    confer_title = paper['title']
+    scores = Amatch::LongestSubsequence.new(confer_title).match(@proceedings_titles)
+    # XXX amatch reports the bytesize (not the length) of the longest common subsequence
+    scores = scores.each_with_index.map {|score, i| score.to_f / confer_title.bytesize}
     closest_paper = @proceedings_papers[scores.each_with_index.max[1]]
+    warn "Best match for '#{confer_title}' is not very close." if scores.max < 0.6
+    two_best_scores = scores.each_with_index.max(2)
+    distance_to_second_best = two_best_scores[0][0] - two_best_scores[1][0]
+    if distance_to_second_best < 0.1
+      warn "Multiple papers match '#{confer_title}' with similar score:"
+      warn "1. #{@proceedings_titles[two_best_scores[0][1]]}"
+      warn "2. #{@proceedings_titles[two_best_scores[1][1]]}"
+      warn 'Choosing the first one.'
+    end
     download_paper(closest_paper[1], closest_paper[0], dir)
   end
 end
